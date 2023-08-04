10fd9a2dd30ef5622a5b15a17aa46548b82e1111
debojyotisb <106902232+debojyotisb@users.noreply.github.com>
2023-08-04 12:47:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import Navbar from './Navbar';
+import Label from './label';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+    <div>
+    <Navbar title= "TextUtils"/>;
+    <Label />;
     </div>
-  );
+    
+    <div  classNameName="App" >
+      <h1> Hello SB</h1>
+     
+    </div>
+  </>
 }
 
 export default App;
